898af5a1e25dcbe960f82a42fe187721bf16c3cc
Boris <borisc1996@gmail.com>
2018-05-19 00:05:57 +0800

    Add Temporary pending table.

---
+++ src/components/stocks/PendingTable.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">Item Name</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Total Price</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in pending">
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.quantity }}</td>
+                    <td>${{ item.price * item.quantity }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        pending() {
+            return this.$store.getters.getPending;
+        }
+    }
+}
+</script>
