ce81b49ab2e41e1b5c5ba1b3b2c7118f32bdf621
Boris <borisc1996@gmail.com>
2018-05-19 00:04:37 +0800

    Add Pending logic. Order Items will go to Pending before Inventory.

---
+++ src/store/modules/pending.js
@@ -0,0 +1,46 @@
+const state = {
+    pending: []
+}
+
+const mutations = {
+    'ADD_TO_PENDING' (state, {stockId, stockName, quantity, stockPrice}) {
+        const record = state.pending.find(element => element.id == stockId);
+        if (record) {
+            record.quantity = record.quantity + quantity
+        } else {
+            state.pending.push({
+                id: stockId,
+                name: stockName,
+                quantity: quantity,
+                // Remember to use the same variable names that are being passed...
+                price: stockPrice
+            })
+        }
+    },
+    'REMOVE_PENDING' (state, {stockId, stockName, quantity}) {
+        let elementPos = state.pending.map(function(x) {return x.id;}).indexOf(stockId);
+        state.pending.splice(elementPos, 1);
+    }
+}
+
+const actions = {
+    addPending({commit}, order) {
+        commit('ADD_TO_PENDING', order)
+    },
+    removePending({commit}, order) {
+        commit('REMOVE_PENDING', order)
+    }
+}
+
+const getters = {
+    getPending(state) {
+        return state.pending;
+    }
+}
+
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+}
