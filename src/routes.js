e38248d52f7fa5304ab2fb0672a8baf8a22b4572
Boris <borisc1996@gmail.com>
2018-05-18 14:14:17 +0800

    Change routing of '/' to redirect to '/home'.

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,11 @@ import Stocks from './components/stocks/Stocks.vue'
 import Form from './components/stocks/Form.vue'
 import Notes from './components/notes/notes.vue'
 
+// path: '/' should lead to site intro.
+
 export const routes = [
-    { path: '/', component: Home },
+    { path: '/', redirect: '/home' },
+    { path: '/home', component: Home },
     { path: '/inventory', component: Portfolio },
     { path: '/order', component: Stocks },
     { path: '/notes', component: Notes }
