539f614e543884d2d998c56d6704394781355214
Boris <borisc1996@gmail.com>
2018-05-20 17:43:31 +0800

    Add additional note for demo purposes.

--- src/data/notes.js
+++ src/data/notes.js
@@ -1,14 +1,20 @@
 export default [
     {
         author:"Susannah D.",
+        date:"May 19, 2018",
+        comment: "Important: We have 5 cakes to make for the 21st! Stock up on some Flour and Icing Sugar!",
+        priority: "Urgent"
+    },
+    {
+        author:"Susannah D.",
         date:"May 15, 2018",
         comment: "We need new baking sheets! They are too burnt to clean.",
-        priority: "Urgent"
+        priority: "Normal"
     },
     {
         author:"Boris C.",
         date:"May 14, 2018",
         comment: "The dough mixer needs 4kg of flour each use. Don't put more or else it will jam.",
-        priority: "Normal"
+        priority: "Low"
     }
 ]
